3445de5eae86bfe6c868c6d62df3d11407b7f9c2
Ezgi <eezgiylmz58@gmail.com>
2024-12-23 11:57:27 +0300

    Database'e devam edildi

--- app/src/main/java/com/ezgiyilmaz/sporfinder/pages/MessagesPage.kt
+++ app/src/main/java/com/ezgiyilmaz/sporfinder/pages/MessagesPage.kt
@@ -11,6 +11,7 @@ import com.ezgiyilmaz.sporfinder.databinding.ActivityMessagesPageBinding
 import com.ezgiyilmaz.sporfinder.models.Messages
 import com.google.firebase.Timestamp
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.FirebaseFirestore
 
 class MessagesPage : AppCompatActivity() {
@@ -25,22 +26,26 @@ class MessagesPage : AppCompatActivity() {
         setContentView(binding.root)
 
         enableEdgeToEdge()
-        auth=FirebaseAuth.getInstance()
+
+        auth = FirebaseAuth.getInstance()
         val matchid = intent.getStringExtra("rivalId") ?: ""
         val matchCreatorId = intent.getStringExtra("creatorUserId") ?: ""
+
         Log.d("matchCreatorId", "onCreate: matchcreatorıd" + matchCreatorId)
+
         val senderId = auth.currentUser!!.uid
         val conversationId = getConversationId(senderId, matchCreatorId)
+
         Log.d("TAG", "matchİdMessgePage: " + matchid)
 
         binding.messagesRecyclerView.layoutManager = LinearLayoutManager(this)
-        messageAdapter = MessagesAdapter(messages,senderId)
+        messageAdapter = MessagesAdapter(messages, senderId)
         binding.messagesRecyclerView.adapter = messageAdapter
         auth = FirebaseAuth.getInstance()
 
 
         binding.sendButton.setOnClickListener {
-            sendMessage(matchCreatorId,senderId, conversationId)
+            sendMessage(matchCreatorId, senderId, conversationId)
         }
     }
 
@@ -53,17 +58,39 @@ class MessagesPage : AppCompatActivity() {
     }
 
 
-    private fun sendMessage(matchCreatorId: String,senderId: String, conversationId: String) {
+    private fun sendMessage(matchCreatorId: String, senderId: String, conversationId: String) {
 
         val messageEditText = binding.messageEditText.text.toString().trim()
 
         if (messageEditText.isEmpty()) {
             Toast.makeText(this, "Lütfen bir mesaj girin.", Toast.LENGTH_LONG).show()
             return
+        } else {
+            val document = db.collection("user").document(senderId)
+            if (senderId!=null) {
+                document.update(
+                    "conversationUsers",
+                    FieldValue.arrayUnion(matchCreatorId)
+                ) // fieldvalue varsa bu id tekrar kaydetmeyi engelliyor
+                    .addOnSuccessListener {
+                        Toast.makeText(this, "conversationUsers kaydedildi", Toast.LENGTH_LONG)
+                            .show()
+
+                    }.addOnFailureListener {
+                        it.localizedMessage
+                    }
+            }
+            val documentReceiver=db.collection("user").document(matchCreatorId)
+            documentReceiver.update("conversationUsers", FieldValue.arrayUnion(senderId)).addOnSuccessListener {
+                Toast.makeText(this, "conversationUsers kaydedildi", Toast.LENGTH_LONG).show()
+
+            }.addOnFailureListener{
+                it.localizedMessage
+            }
         }
 
         val message = Messages(
-            receiver =matchCreatorId,
+            receiver = matchCreatorId,
             sendId = senderId,
             messages = messageEditText,
             timestamp = Timestamp.now()
@@ -81,5 +108,4 @@ class MessagesPage : AppCompatActivity() {
                 Toast.makeText(this, it.localizedMessage, Toast.LENGTH_LONG).show()
             }
     }
-
 }
